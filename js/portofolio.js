d465d65833cad46f2e747b9258ad04bbfeee1c46
Thomas <120122755+thomastour@users.noreply.github.com>
2024-09-29 10:58:15 +0300

    portofolioV2

--- js/portofolio.js
+++ js/portofolio.js
@@ -1,33 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const modeToggle = document.getElementById("mode-toggle");
-  const body = document.body;
+document.addEventListener('DOMContentLoaded', function() {
 
-  // Check if the user's preference is already set
-  if (localStorage.getItem("colorMode") === "dark") {
-      body.classList.add("dark-mode");
-  } else if (localStorage.getItem("colorMode") === "bright") {
-      body.classList.add("bright-mode");
-      
-     
-      
-  }
-
-  modeToggle.addEventListener("click", () => {
-      if (body.classList.contains("dark-mode")) {
-          body.classList.remove("dark-mode");
-          modeToggle.querySelector("i").classList.toggle("fa-moon");
-          body.classList.add("bright-mode");
-          modeToggle.querySelector("i").classList.toggle("fa-sun");
-          localStorage.setItem("colorMode", "bright");
-      } else {
-        modeToggle.querySelector("i").classList.toggle("fa-sun");
-          body.classList.remove("bright-mode");
-          modeToggle.querySelector("i").classList.toggle("fa-moon");
-          body.classList.add("dark-mode");
-          localStorage.setItem("colorMode", "dark");
-      }
-  });
-
-});
-
+    const date = new Date().getFullYear();
+    console.log(date);
+    document.getElementById('date').innerHTML = `&copy;${date} T|T `; 
+})
 
